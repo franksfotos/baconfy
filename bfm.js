80137bd06d688bb6c3a625a295a7c4772cd5c995
franksfotos <frankie@htcdesire.fritz.box>
2014-03-13 23:24:09 +0100

    initial upload 1st draft

---
+++ bfm.js
@@ -0,0 +1,44 @@
+function baconfy(bfnr){
+	
+
+	var fileNameNr1 = "./img/" + bfnr[0] + ".png"
+	var fileNameNr2 = "./img/" + bfnr[1] + ".png"
+
+	console.log(fileNameNr1)
+	var objCanvas = document.getElementById('baconfied');
+	
+	objCanvas.width = objCanvas.width;
+
+	if(objCanvas.getContext){
+		var context = objCanvas.getContext('2d');	
+		var imageObj1 = new Image();
+		var imageObj2 = new Image();
+
+		imageObj1.onload = function() {
+			context.drawImage(imageObj1,0,0,imageObj1.width*.4,imageObj1.height*.4)
+		};
+		imageObj1.src = fileNameNr1;
+
+		imageObj2.onload = function() {
+			context.drawImage(imageObj2,this.width*.4,0,imageObj2.width*.4,imageObj2.height*.4)
+		};
+		imageObj2.src = fileNameNr2;
+	}
+
+	
+}
+
+function FocusOnInput()
+{
+     document.getElementById("bfnr").focus();
+     document.getElementById('download').addEventListener('click', download, false	);
+}
+
+
+function downloadCanvas() {
+    var dt = objCanvas.toDataURL();
+    this.href = dt; //this may not work in the future..
+}
+
+
+
